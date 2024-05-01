503ad1e6eedad03c15c1fee2beb578e3a3977431
albani <albani@prueba.com>
2024-05-01 18:57:26 -0400

    Pagina de retiros de bienestar del cliente

--- src/pages/UserPlans/UserPlans.jsx
+++ src/pages/UserPlans/UserPlans.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import "./UserPlans.css";
 import ContainerHome from "../../components/containerHome/containerHome.jsx";
+import TableDetails from "../../components/Table/TableDetails.jsx";
 
 const UserPlans = () => {
+  const userName = "Test User";
   return (
     <div className="userPlans-page">
       <ContainerHome>
         <div className="userPlans-container">
-          <h1>PLANES del usuario</h1>
+          <h1 className="userPlans-title">
+            Tus Planes de Retiro de Bienestar {userName}
+          </h1>{" "}
+          <div className="user-table">
+            <TableDetails />
+          </div>
         </div>
       </ContainerHome>
     </div>
